7bdf93d27441d4ed95e82882cfb761c19387208b
majames <majames91@gmail.com>
2015-07-05 08:49:53 +1000

    Completed tutorial: accessing child components (stuff within tags)

--- egg_head/custom.jsx
+++ egg_head/custom.jsx
@@ -1,69 +1,20 @@
-var PropTypesApp = React.createClass({
-  propTypes: {
-    txt: React.PropTypes.string,
-    cat: React.PropTypes.number.isRequired
-  },
-
-  getDefaultProps: function() {
-    return {
-      txt: 'this is a default prop',
-      cat: 0
-    };
-  },
-
+var App = React.createClass({
   render: function() {
-    var txt = this.props.txt;
-    return (
-      <div>
-        <b>BOLD</b>
-        <h1>{txt}</h1>
-      </div>
-    );
+    return <Button>I <Heart /> React</Button>;
   }
 });
 
-var StateApp = React.createClass({
-  getInitialState: function() {
-    return {
-      red: 0,
-      green: 0,
-      blue: 0
-    };
-  },
-
-  update: function(e) {
-    this.setState({
-      red: this.refs.red.refs.inp.getDOMNode().value,
-      green: this.refs.green.refs.inp.getDOMNode().value,
-      blue: this.refs.blue.refs.inp.getDOMNode().value,
-    });
-  },
-
+var Button = React.createClass({
   render: function() {
-    return (
-      <div>
-        <label>{this.state.red}</label>
-        <Slider ref="red" update={this.update} />
-
-        <label>{this.state.green}</label>
-        <Slider ref="green" update={this.update} />
-
-        <label>{this.state.blue}</label>
-        <Slider ref="blue" update={this.update} />
-      </div>
-    );
+    return <button>{this.props.children}</button>;
   }
 });
 
-var Slider = React.createClass({
+var Heart = React.createClass({
   render: function() {
-    return (
-      <div>
-        <input ref="inp" type="range" min="0" max="255" onChange={this.props.update} />
-      </div>
-    );
+    return <span>LOVE</span>;
   }
 });
 
 
-React.render(<StateApp />, document.body);
+React.render(<App />, document.body);
