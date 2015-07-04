bbf6df19265b707c235399518e680799871810d1
majames <majames91@gmail.com>
2015-07-05 09:15:37 +1000

    Tutorial: component will mount, did mount and will unmount functions explored

--- egg_head/custom.jsx
+++ egg_head/custom.jsx
@@ -1,20 +1,52 @@
 var App = React.createClass({
   render: function() {
-    return <Button>I <Heart /> React</Button>;
+    return (
+      <div>
+        <button onClick={this.mount}>Mount the Component</button>
+        <button onClick={this.unmount}>Unmount the component</button>
+        <div id="target"></div>
+      </div>
+    );
+  },
+
+  mount: function() {
+    React.render(<Button />, document.getElementById('target'));
+  },
+
+  unmount: function() {
+    React.unmountComponentAtNode(document.getElementById('target'));
   }
 });
 
 var Button = React.createClass({
+  getInitialState: function() {
+    return {
+      val: 0
+    };
+  },
+
+  componentWillMount: function() {
+    console.log('Mounting... Called before initial render');
+  },
+
   render: function() {
-    return <button>{this.props.children}</button>;
+    console.log('rendering');
+    return <button onClick={this.update}>{this.state.val}</button>;
+  },
+
+  componentDidMount: function() {
+    console.log('Mounted. Called after initial render');
+  },
+
+  componentWillUnmount: function() {
+    console.log('Component unmounting...');
+  },
+
+  update: function() {
+    this.setState({
+      val: this.state.val + 1
+    });
   }
 });
-
-var Heart = React.createClass({
-  render: function() {
-    return <span>LOVE</span>;
-  }
-});
-
 
 React.render(<App />, document.body);
