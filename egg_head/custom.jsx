719adc0f02a178248d1bc7f2160d43f8f581e18e
majames <majames91@gmail.com>
2015-07-05 10:09:19 +1000

    Tutorial: discovered why mounting and unmounting methods are useful

--- egg_head/custom.jsx
+++ egg_head/custom.jsx
@@ -1,9 +1,46 @@
+var Button = React.createClass({
+  getInitialState: function() {
+    return {
+      val: 0
+    };
+  },
+
+  componentWillMount: function() {
+    console.log('Mounting... Useful for setting up state each time component is mounted');
+
+    this.setState({
+      multiplier: 2
+    });
+  },
+
+  render: function() {
+    console.log('rendering');
+    return <button onClick={this.update}>{this.state.val * this.state.multiplier}</button>;
+  },
+
+  componentDidMount: function() {
+    console.log('Mounted. Useful for starting timed events');
+    this.inc =setInterval(this.update, 1000);
+  },
+
+  componentWillUnmount: function() {
+    console.log('Unmounted. Useful for ensuring no state is set after component is unmounted');
+    clearInterval(this.inc);
+  },
+
+  update: function() {
+    this.setState({
+      val: this.state.val + 1
+    });
+  }
+});
+
 var App = React.createClass({
   render: function() {
     return (
       <div>
-        <button onClick={this.mount}>Mount the Component</button>
-        <button onClick={this.unmount}>Unmount the component</button>
+        <button onClick={this.mount}>Mount</button>
+        <button onClick={this.unmount}>Unmount</button>
         <div id="target"></div>
       </div>
     );
@@ -16,37 +53,8 @@ var App = React.createClass({
   unmount: function() {
     React.unmountComponentAtNode(document.getElementById('target'));
   }
-});
 
-var Button = React.createClass({
-  getInitialState: function() {
-    return {
-      val: 0
-    };
-  },
 
-  componentWillMount: function() {
-    console.log('Mounting... Called before initial render');
-  },
-
-  render: function() {
-    console.log('rendering');
-    return <button onClick={this.update}>{this.state.val}</button>;
-  },
-
-  componentDidMount: function() {
-    console.log('Mounted. Called after initial render');
-  },
-
-  componentWillUnmount: function() {
-    console.log('Component unmounting...');
-  },
-
-  update: function() {
-    this.setState({
-      val: this.state.val + 1
-    });
-  }
 });
 
 React.render(<App />, document.body);
