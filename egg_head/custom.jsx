590ab53d4a19ab1552105f05439f52622472f65c
majames <majames91@gmail.com>
2015-07-09 19:55:29 +1000

    React warm-up tutorial

--- egg_head/custom.jsx
+++ egg_head/custom.jsx
@@ -1,40 +1,22 @@
-var PersonTable = React.createClass({
+var App = React.createClass({
 
   getInitialState: function() {
     return {
-      data: [
-        { id: 1, fname: 'Michael', lname: 'James' },
-        { id: 2, fname: 'Annika', lname: 'James' },
-        { id: 3, fname: 'Andrew', lname: 'James' },
-        { id: 4, fname: 'Haimish', lname: 'James' },
-      ]
+      data: [1, 2, 3, 4, 5, 6, 7]
     };
   },
 
   render: function() {
-    var rows = this.state.data.map(function(value, index) {
-      return <PersonRow data={value} key={value.id} />;
+    var items = this.state.data.map(function(datum) {
+      return <li>{datum}</li>;
     });
 
     return (
-      <table>{rows}</table>
+      <div>
+        {items}
+      </div>
     );
   }
-
 });
 
-var PersonRow = React.createClass({
-
-  render: function() {
-    return (
-      <tr>
-        <td>{this.props.data.id}</td>
-        <td>{this.props.data.fname}</td>
-        <td>{this.props.data.lname}</td>
-      </tr>
-    );
-  }
-
-});
-
-React.render(<PersonTable />, document.body);
+React.render(<App />, document.body);
