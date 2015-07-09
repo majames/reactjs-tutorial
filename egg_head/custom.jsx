c2a14384511e57c4d4ddc9bd1180f1585de928aa
majames <majames91@gmail.com>
2015-07-09 20:15:03 +1000

    Explored React spread operator and the use of React.render in React 0.12

--- egg_head/custom.jsx
+++ egg_head/custom.jsx
@@ -1,21 +1,16 @@
 var App = React.createClass({
 
-  getInitialState: function() {
-    return {
-      data: [1, 2, 3, 4, 5, 6, 7]
-    };
-  },
-
   render: function() {
-    var items = this.state.data.map(function(datum) {
-      return <li>{datum}</li>;
-    });
-
     return (
-      <div>
-        {items}
-      </div>
+      <Title className="red" id="blue" value="green" />
     );
+  }
+
+});
+
+var Title = React.createClass({
+  render: function() {
+    return <h1 {...this.props}>Using the spread operator</h1>;
   }
 });
 
